a1d32464c4dfda7c8b6f563cc2a74a009822a91c
meowseal <hotdogtoiletcleaner@gmail.com>
2025-10-27 11:50:29 -0500

    Create utils.java

---
+++ utils.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class utils {
+    
+
+    Scanner in = new Scanner(System.in);   
+    
+    
+    private static void print(Object text) {
+        System.out.print(text);
+    }
+
+    private static void println(Object text) {
+        System.out.println(text);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        print("Funny meter: ");
+        print(0);
+        println("%");
+        print("Hello World");
+    
+    }
+}
