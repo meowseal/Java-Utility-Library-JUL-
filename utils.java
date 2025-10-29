e8a9a7c9857859db4e4865ecefbcc0edb9061b8e
meowseal <hotdogtoiletcleaner@gmail.com>
2025-10-29 11:35:46 -0500

    Update utils.java

--- utils.java
+++ utils.java
@@ -1,5 +1,7 @@
+// Import Scanner for input
 import java.util.Scanner;
 
+// File Class
 public class utils {
     // Text functions
 
@@ -7,43 +9,43 @@ public class utils {
     Scanner input = new Scanner(System.in);   
     
     // Prints without a new line
-    private static void print(Object text) {
+    public static void print(Object text) {
         System.out.print(text);
     }
     // Prints with new line
-    private static void println(Object text) {
+    public static void println(Object text) {
         System.out.println(text);
     }
     // Gets length of text
-    private static int len(String text) {
+    public static int len(String text) {
         return text.length();
     }    
 
     // Math functions
 
     // Absolute
-    private static double abs(double Input) {
+    public static double abs(double Input) {
         return Math.abs(Input);
         }
     // Square Root
-    private static double sqrt(double Input) {
+    public static double sqrt(double Input) {
         return Math.sqrt(Input);
         }
 
     // Rounding
 
     // Rounding to the nearest whole number
-    private static double round(double Input) {
+    public static double round(double Input) {
         return Math.round(Input);
         }
 
     // Rounding to the lowest whole number
-    private static double floor(double Input) {
+    public static double floor(double Input) {
         return Math.floor(Input);
         }
 
     // Rounding to the highest whole number
-    private static double ceiling(double Input) {
+    public static double ceiling(double Input) {
         return Math.ceil(Input);
         }  
 
@@ -51,37 +53,37 @@ public class utils {
 
 
     // Sine in Radians
-    private static double sin(double Input) {
+    public static double sin(double Input) {
         return Math.sin(Input);
         }
 
    // Cosine in Radians
-    private static double cos(double Input) {
+    public static double cos(double Input) {
         return Math.cos(Input);
         }
 
    // Arc Tangent in Radians
-    private static double tan(double Input) {
+    public static double tan(double Input) {
         return Math.tan(Input);
         } 
 
     // Arc Sine in Radians
-    private static double asin(double Input) {
+    public static double asin(double Input) {
         return Math.asin(Input);
         }
 
    // Arc Cosine in Radians
-    private static double acos(double Input) {
+    public static double acos(double Input) {
         return Math.acos(Input);
         }
 
    // Tangent in Radians
-    private static double atan(double Input) {
+    public static double atan(double Input) {
         return Math.atan(Input);
         } 
 
    // Atan2 in Radians
-    private static double atan2(double Input,double Input2) {
+    public static double atan2(double Input,double Input2) {
         return Math.atan2(Input,Input2);
         } 
 
@@ -89,15 +91,14 @@ public class utils {
     // Conversions
     
     // Convert to Degrees
-    private static double deg(double Input) {
+    public static double deg(double Input) {
         return Math.toDegrees(Input);
         }      
 
     // Convert to Degrees
-    private static double rad(double Input) {
+    public static double rad(double Input) {
         return Math.toRadians(Input);
         } 
 
-
-
+    }
 }
