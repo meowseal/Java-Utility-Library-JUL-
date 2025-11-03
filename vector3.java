3524c048018a8335e7ced21c15688035cb76b6c0
meowseal <hotdogtoiletcleaner@gmail.com>
2025-11-03 12:05:37 -0600

    Create vector3.java
    Learned how to make custom data types with objects

---
+++ vector3.java
@@ -0,0 +1,30 @@
+public class vector3 
+{
+    // initialize data & types
+    private double a,b,c;
+
+    // set values and attributes
+    public vector3(double x, double y, double z)
+    {
+        a = (double)x;        
+        b = (double)y;
+        c = (double)z;
+    }
+    // return x
+    public double x()
+    { 
+        return a;
+    }
+    // return y
+    public double y()
+    {
+        return b;
+    }
+    // return z
+    public double z()
+    {
+        return c;
+    }
+  
+    // More methods to be added
+}
