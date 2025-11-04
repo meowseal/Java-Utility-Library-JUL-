5f9708372dccfafa4765ae01bbc148dfc4d5a96e
meowseal <hotdogtoiletcleaner@gmail.com>
2025-11-04 11:19:29 -0600

    Update vector3.java
    added new method + comments

--- main/vector3.java
+++ main/vector3.java
@@ -11,12 +11,28 @@ public class vector3
         z = c;
     }
 
-    // return modified vector3 as int
+    /**
+    * Method returns what the vector3 would be if it was converted to int.
+    * It doesn't directly convert the data into int due to initializer type naming.
+    * Please fix to it directly turning it into any type using Object type.
+    */
+
     public vector3 toInt()
     {
         vector3 intVector = new vector3((int)x,(int)y,(int)z);
         return intVector;
     }
 
+    /**
+    * Method sets data to new data when inputed
+    * Doesn't return data so you have to print it manually by calling for the variables.
+    */
+
+    public void replace(double a, double b, double c)
+    {
+        x = a;        
+        y = b;
+        z = c;        
+    }
 
 }
